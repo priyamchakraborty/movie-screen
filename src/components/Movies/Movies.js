5b0a17f604e3148970daa42d4a9397f8deb5b4c3
Priyam Chakraborty <83344214+priyamchakraborty@users.noreply.github.com>
2024-08-08 12:31:26 +0530

    CSS in list

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -6,7 +6,7 @@ function Movies() {
   const getMovie = async () => {
     try {
       const res = await fetch(
-        "https://api.themoviedb.org/3/discover/movie?api_key=f413b4fc01fc9577f826147de2d85b1d",
+        "https://api.themoviedb.org/3/discover/movie?api_key=f413b4fc01fc9577f826147de2d85b1d&include_adult=true&include_video=true&language=en-US&page=5&sort_by=popularity.desc",
       );
       const json = await res.json();
       setMovieList(json.results);
@@ -25,11 +25,13 @@ function Movies() {
       {movieList.map((movie) => {
         return (
           <div className={styles.card}>
-            <img
-              key={movie.id}
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.original_title}
-            />
+            <div className={styles.moviePoster}>
+              <img
+                key={movie.id}
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.original_title}
+              />
+            </div>
             <div className={styles.overlay}>
               <h4>{movie.title}</h4>
               <div>Rating: {movie.vote_average}</div>
