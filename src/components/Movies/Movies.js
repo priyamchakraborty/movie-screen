bae759605a9df1794d2faa5c8249a92deff6027a
Priyam Chakraborty <83344214+priyamchakraborty@users.noreply.github.com>
2024-08-10 23:00:58 +0530

    WIP

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Movies.module.scss";
+import logo from "./../Images/movie-screen-logo.png";
 
 function Movies() {
   const [movieList, setMovieList] = useState([]);
@@ -20,7 +21,7 @@ function Movies() {
     getMovie();
   }, []);
 
-  const openModal = (movie) => {
+  const openModal = movie => {
     setSelectedMovie(movie);
   };
 
@@ -31,72 +32,84 @@ function Movies() {
   console.log(movieList);
 
   return (
-    <div className={styles.listContainer}>
-      {movieList.map(
-        ({
-          id,
-          poster_path,
-          original_title,
-          title,
-          vote_average,
-          release_date,
-          overview,
-        }) => {
-          const dateStr = release_date;
-          const date = new Date(dateStr);
-          const formattedDate = date.toLocaleDateString("en-GB", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          });
-          const rating = vote_average.toFixed(1);
-          const ratingPercent = rating * 10;
-          return (
-            <div
-              key={id}
-              className={styles.card}
-              onClick={() => openModal({ title, overview })}
-            >
-              <div className={styles.moviePoster}>
-                <img src={`${image_URL}${poster_path}`} alt={original_title} />
-              </div>
-              <div className={styles.info}>
-                <div>
-                  <h4>{title}</h4>
-                  <p>{formattedDate}</p>
-                  <p>{overview}</p>
+    <>
+      <header className={styles.headerContainer}>
+        <img src={logo} />
+      </header>
+      <div className={styles.listContainer}>
+        {movieList.map(
+          ({
+            id,
+            poster_path,
+            original_title,
+            title,
+            vote_average,
+            release_date,
+            overview,
+            backdrop_path,
+          }) => {
+            const dateStr = release_date;
+            const date = new Date(dateStr);
+            const formattedDate = date.toLocaleDateString("en-GB", {
+              day: "2-digit",
+              month: "long",
+              year: "numeric",
+            });
+            const rating = vote_average.toFixed(1);
+            const ratingPercent = rating * 10;
+            return (
+              <div
+                key={id}
+                className={styles.card}
+                onClick={() =>
+                  openModal({ title, overview, backdrop_path, formattedDate })
+                }
+              >
+                <div className={styles.moviePoster}>
+                  <img
+                    src={`${image_URL}${poster_path}`}
+                    alt={original_title}
+                  />
                 </div>
-                <div className={styles.progressRateContainer}>
-                  <div className={styles.rating}>{rating}</div>
-                  <div
-                    className={styles.progressBar}
-                    style={{
-                      background: `radial-gradient(
+                <div className={styles.info}>
+                  <div>
+                    <h4>{title}</h4>
+                  </div>
+                  <div className={styles.progressRateContainer}>
+                    <div className={styles.rating}>{rating}</div>
+                    <div
+                      className={styles.progressBar}
+                      style={{
+                        background: `radial-gradient(
                                     closest-side,
                                     #282c34 79%,
                                     transparent 80% 100%
                                 ),
                                 conic-gradient(#dddd3d ${ratingPercent}%, grey 0)`,
-                    }}
-                  ></div>
+                      }}
+                    ></div>
+                  </div>
                 </div>
               </div>
+            );
+          }
+        )}
+        {selectedMovie && (
+          <div className={styles.modalOverlay}>
+            <div className={styles.modal}>
+              <button onClick={closeModal} className={styles.closeButton} />
+              <div className={styles.overlay}></div>
+              <img src={`${image_URL}${selectedMovie.backdrop_path}`} />
+              <div className={styles.modalBody}>
+                <h2>{selectedMovie.title}</h2>
+                <p>{selectedMovie.overview}</p>
+                <p>Release: {selectedMovie.formattedDate}</p>
+              </div>
             </div>
-          );
-        },
-      )}
-      {selectedMovie && (
-        <div className={styles.modalOverlay}>
-          <div className={styles.modal}>
-            <h2>{selectedMovie.title}</h2>
-            <p>{selectedMovie.overview}</p>
-            <button onClick={closeModal} className={styles.closeButton}>
-              Close
-            </button>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 }
 
